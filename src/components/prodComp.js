83b318b05b8ed23fe0892fe1307e686cee2e56c4
hibaelo <elouafihiba1@gmail.com>
2022-11-10 11:03:42 +0100

    first commit

---
+++ src/components/prodComp.js
@@ -0,0 +1,14 @@
+import './product.css'
+function ProductComponent(){
+    return(
+    <div  className='prodList'>
+        <ul>
+        <li>Product:</li>
+        <li>Categorie: Informatique</li>
+        <li>Prix: 10 000.99 Dh</li>
+
+        </ul>
+    </div>
+    )
+}
+export default  ProductComponent ;
